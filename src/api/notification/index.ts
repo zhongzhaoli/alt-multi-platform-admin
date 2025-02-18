179836d859ab4a0bb32fbe015b6391994fd1522d
zhongzhaoli <525966315@qq.com>
2025-02-18 10:23:04 +0800

    feat: 信息通知

---
+++ src/api/notification/index.ts
@@ -0,0 +1,28 @@
+import { ResponsePageJson } from '@/config/request';
+import { request } from '@/utils/request';
+
+export interface GetListDto {
+  page: number;
+  page_size?: number;
+  shop_id?: string;
+  created_date?: string;
+}
+
+export interface NotificationProps {
+  created_at: string;
+  id: number;
+  message: 0 | 1;
+  notification: 0 | 1;
+  shop_id: string;
+  shop_name: string;
+}
+
+export function getNotificationList(
+  params: GetListDto
+): Promise<ResponsePageJson<NotificationProps>> {
+  return request({
+    url: '/walmart/message/notify/list',
+    method: 'get',
+    params
+  });
+}
--- src/views/walmartNotification/index.vue
+++ src/views/walmartNotification/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="filterBox">
-      <FilterContainer v-model="filterValue" :columns="config.filterColumns">
+      <FilterContainer v-model="filterValue" :columns="config.filterColumns" @submit="getListFun">
         <template #shop_id="{ form, row }">
           <SelectWalmartStore v-model="form[row.prop]" multiple @change="getListFun" />
         </template>
@@ -23,6 +23,8 @@
         :pagination="{
           total
         }"
+        @page-change="getListFun"
+        @table-refresh="getListFun"
       >
         <template #handle-left>
           <span class="testText"> PS：测试数据 </span>
@@ -36,29 +38,32 @@ import TsxElementTable from 'tsx-element-table';
 import FilterContainer from '@/components/FilterContainer/index.vue';
 import SelectWalmartStore from '@/components/SelectWalmartStore/index.vue';
 import * as config from './config';
+import { getNotificationList, type NotificationProps } from '@/api/notification';
 import { shallowRef, ref } from 'vue';
 import { PAGE, PAGE_SIZE } from '@/constants/app';
 
 const filterValue = ref<Partial<config.FilterDto>>({});
-const tableData = shallowRef([
-  {
-    shop_name: 'Pengchengming',
-    message: true,
-    notification: false,
-    created_at: '2021-09-01 12:00:00'
-  },
-  {
-    shop_name: 'XUNXUNKEJI',
-    message: false,
-    notification: false,
-    created_at: '2021-09-01 12:00:00'
-  }
-]);
+const tableData = shallowRef<NotificationProps[]>([]);
 const loading = shallowRef(false);
 const page = shallowRef(PAGE);
 const pageSize = shallowRef(PAGE_SIZE);
-const total = 0;
-const getListFun = async () => {};
+const total = shallowRef(0);
+const getListFun = async () => {
+  loading.value = true;
+  try {
+    const { data } = await getNotificationList({
+      page: page.value,
+      page_size: pageSize.value,
+      ...filterValue.value
+    });
+    tableData.value = data?.list || [];
+    total.value = data?.total || 0;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    loading.value = false;
+  }
+};
 
 getListFun();
 </script>
