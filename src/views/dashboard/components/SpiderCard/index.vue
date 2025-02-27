32ab50e496fb10642922100d1b8cfcfe1bcda330
zhongzhaoli <525966315@qq.com>
2025-02-27 15:23:07 +0800

    feat: 上架看板和爬虫看板

--- src/views/dashboard/components/SpiderCard/index.vue
+++ src/views/dashboard/components/SpiderCard/index.vue
@@ -2,6 +2,7 @@
   <div class="spiderBox cardBox">
     <div class="titleBox">
       <div class="title flex-center">爬虫看板</div>
+      <el-button link type="primary" @click="toDetail">查看详情</el-button>
     </div>
     <div class="body">
       <SpiderEchart :table-data="tableData" :x-data="last7Days" :loading="false" />
@@ -13,6 +14,8 @@ import SpiderEchart from './SpiderEchart.vue';
 import moment from 'moment-timezone';
 import { getSpiderList } from '@/api/spider/index';
 import { ref, shallowRef, unref } from 'vue';
+import { useRouter } from 'vue-router';
+const router = useRouter();
 // 获取当前日期
 const today = moment();
 
@@ -24,6 +27,10 @@ for (let i = 0; i < 7; i++) {
   const date = today.clone().subtract(i, 'days'); // 从当前日期往前推 i 天
   last7Days.value.unshift(date.format('YYYY-MM-DD')); // 格式化日期并添加到数组
 }
+
+const toDetail = () => {
+  router.push('/spider');
+};
 
 const tableData = shallowRef<number[]>([]);
 const loading = shallowRef(true);
