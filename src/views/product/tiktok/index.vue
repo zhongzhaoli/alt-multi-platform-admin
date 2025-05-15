1d4f6ed043f70dffab536dfbc38fc4ca11709345
zhongzhaoli <525966315@qq.com>
2025-05-15 11:14:31 +0800

    feat: TK优化

--- src/views/product/tiktok/index.vue
+++ src/views/product/tiktok/index.vue
@@ -7,8 +7,8 @@
         @submit="getListFun"
         @reset="getListFun"
       >
-        <template #shop_id="{ form, row }">
-          <SelectTiktokStore v-model="form[row.prop]" multiple @change="getListFun" />
+        <template #shop_id="{ form, row, change }">
+          <SelectTiktokStore v-model="form[row.prop]" multiple @change="change" />
         </template>
       </FilterContainer>
     </div>
